c3e5ee5b4d0ab36b3d8726c0ca6b8c5744d8a9c7
willyanfx <willyan.fx@gmail.com>
2022-01-20 11:15:52 -0300

    add svg gift image

--- components/Gift.tsx
+++ components/Gift.tsx
@@ -2,13 +2,355 @@ import styles from "../styles/Gift.module.css";
 
 function Gift(): JSX.Element {
   return (
-    <div className={styles.gift}>
-      <div className={styles.lid}></div>
-      <div className={styles.body}></div>
-      <div className={styles.lasso__first}></div>
-      <div className={styles.lasso__second}></div>
-    </div>
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      width='221.59'
+      height='234.058'
+      viewBox='0 0 221.59 234.058'
+    >
+      <defs>
+        <radialGradient
+          id='radial-gradient'
+          cx='-0.227'
+          cy='0.15'
+          r='1.247'
+          gradientTransform='translate(-0.183) scale(1.365 1)'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#c90000' />
+          <stop offset='1' stopColor='#730000' />
+        </radialGradient>
+        <radialGradient
+          id='radial-gradient-2'
+          cx='-0.153'
+          cy='-0.046'
+          r='1.315'
+          gradientTransform='translate(-0.183) scale(1.366 1)'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#fff2d9' />
+          <stop offset='0.01' stopColor='#ffddb7' />
+          <stop offset='0.03' stopColor='#ffc28d' />
+          <stop offset='0.06' stopColor='#ffaa67' />
+          <stop offset='0.08' stopColor='#ff9647' />
+          <stop offset='0.11' stopColor='#ff862d' />
+          <stop offset='0.13' stopColor='#ff7a19' />
+          <stop offset='0.17' stopColor='#ff710b' />
+          <stop offset='0.21' stopColor='#ff6c02' />
+          <stop offset='0.27' stopColor='#ff6a00' />
+          <stop offset='1' stopColor='#e81e00' />
+        </radialGradient>
+        <radialGradient
+          id='radial-gradient-3'
+          cx='0.083'
+          cy='0.182'
+          r='2.568'
+          gradientTransform='translate(0.064) scale(0.873 1)'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#d70000' />
+          <stop offset='1' stopColor='#8a0000' />
+        </radialGradient>
+        <radialGradient
+          id='radial-gradient-4'
+          cx='-0.102'
+          cy='-0.402'
+          r='2.029'
+          gradientTransform='translate(0.064) scale(0.873 1)'
+          xlinkHref='#radial-gradient-2'
+        />
+        <radialGradient
+          id='radial-gradient-5'
+          cx='-0.073'
+          cy='0.33'
+          r='1.612'
+          gradientTransform='translate(0.211) scale(0.577 1)'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ff9a42' />
+          <stop offset='1' stopColor='#ff543d' />
+        </radialGradient>
+        <linearGradient
+          id='linear-gradient'
+          x1='0.367'
+          y1='0.975'
+          x2='0.633'
+          y2='0.025'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ffe98d' />
+          <stop offset='1' stopColor='#ff9e48' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-2'
+          x1='0.46'
+          y1='0.591'
+          x2='0.54'
+          y2='0.41'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ffb05c' />
+          <stop offset='0.15' stopColor='#ffa152' />
+          <stop offset='0.38' stopColor='#ff9147' />
+          <stop offset='0.64' stopColor='#ff8840' />
+          <stop offset='1' stopColor='#ff853e' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-3'
+          x1='0.136'
+          y1='0.869'
+          x2='0.738'
+          y2='0.091'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ffb763' />
+          <stop offset='0.05' stopColor='#ffc26f' />
+          <stop offset='0.15' stopColor='#ffde8d' />
+          <stop offset='0.16' stopColor='#ffe292' />
+          <stop offset='0.29' stopColor='#ffc471' />
+          <stop offset='0.36' stopColor='#ffb763' />
+          <stop offset='0.47' stopColor='#ffc771' />
+          <stop offset='0.64' stopColor='#ffda80' />
+          <stop offset='0.81' stopColor='#ffe58a' />
+          <stop offset='1' stopColor='#ffe98d' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-4'
+          x1='0.247'
+          y1='0.14'
+          x2='0.686'
+          y2='0.925'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ff7529' />
+          <stop offset='0.15' stopColor='#ff7b2e' />
+          <stop offset='0.36' stopColor='#ff8c3d' />
+          <stop offset='0.59' stopColor='#ffa855' />
+          <stop offset='0.7' stopColor='#ffb763' />
+          <stop offset='0.92' stopColor='#ffb460' />
+          <stop offset='1' stopColor='#ffb05c' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-5'
+          x1='0.282'
+          y1='0.07'
+          x2='0.827'
+          y2='0.886'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ffe98d' />
+          <stop offset='0.2' stopColor='#ffd37b' />
+          <stop offset='0.41' stopColor='#ffc36d' />
+          <stop offset='0.66' stopColor='#ffba65' />
+          <stop offset='1' stopColor='#ffb763' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-6'
+          x1='0.419'
+          y1='0.058'
+          x2='0.581'
+          y2='0.825'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ffe186' />
+          <stop offset='0.11' stopColor='#ffd77f' />
+          <stop offset='0.35' stopColor='#ffc774' />
+          <stop offset='0.62' stopColor='#ffbe6e' />
+          <stop offset='1' stopColor='#ffbb6c' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-7'
+          x1='0.378'
+          y1='0.315'
+          x2='0.622'
+          y2='0.685'
+          gradientUnits='objectBoundingBox'
+        >
+          <stop offset='0' stopColor='#ff762c' />
+          <stop offset='0.24' stopColor='#ff7c31' />
+          <stop offset='0.56' stopColor='#ff8d3f' />
+          <stop offset='0.92' stopColor='#ffa956' />
+          <stop offset='1' stopColor='#ffb05c' />
+        </linearGradient>
+        <linearGradient
+          id='linear-gradient-8'
+          x1='0.279'
+          y1='0.116'
+          x2='0.772'
+          y2='0.809'
+          xlinkHref='#linear-gradient-5'
+        />
+      </defs>
+      <g id='gift' transform='translate(-71.63 -5.692)'>
+        <path
+          id='Path_71'
+          data-name='Path 71'
+          d='M286.46,97.69v82L182.42,239.75v-82Z'
+          fill='url(#radial-gradient)'
+        />
+        <path
+          id='Path_72'
+          data-name='Path 72'
+          d='M182.42,157.75v82L78.39,179.69v-82Z'
+          fill='url(#radial-gradient-2)'
+        />
+        <g id='Group_2' data-name='Group 2'>
+          <path
+            id='Path_73'
+            data-name='Path 73'
+            d='M293.22,96.69V129.4l-110.8,63.97V160.66Z'
+            fill='url(#radial-gradient-3)'
+          />
+          <path
+            id='Path_74'
+            data-name='Path 74'
+            d='M182.42,160.66v32.71L71.63,129.4V96.69Z'
+            fill='url(#radial-gradient-4)'
+          />
+          <path
+            id='Path_75'
+            data-name='Path 75'
+            d='M71.63,96.69,182.42,32.73l110.8,63.96-110.8,63.97Z'
+            fill='url(#radial-gradient-5)'
+          />
+        </g>
+        <path
+          id='Path_76'
+          data-name='Path 76'
+          d='M226.63,214.23l15.63-9.02V158.83l-15.63,9.02Z'
+          fill='#ff9e4d'
+        />
+        <path
+          id='Path_77'
+          data-name='Path 77'
+          d='M138.22,214.23l-15.63-9.02V158.83l15.63,9.02Z'
+          fill='#ffc46b'
+        />
+        <path
+          id='Path_78'
+          data-name='Path 78'
+          d='M230,60.2,119.21,124.16l15.63,9.02L245.63,69.22Z'
+          fill='#ffe589'
+        />
+        <path
+          id='Path_79'
+          data-name='Path 79'
+          d='M134.84,165.9l-15.63-9.02V124.16l15.63,9.02Zm0-105.7,110.79,63.96-15.62,9.02L119.21,69.22l15.63-9.02Z'
+          fill='#ffd87f'
+        />
+        <path
+          id='Path_80'
+          data-name='Path 80'
+          d='M230.01,165.9l15.62-9.02V124.16l-15.62,9.02Z'
+          fill='#ffbc67'
+        />
+        <g id='ribbon'>
+          <g id='Group_6' data-name='Group 6'>
+            <path
+              id='Path_81'
+              data-name='Path 81'
+              d='M117.43,108.72l26.74,15.44-2.86-3.57-26.73-15.44Z'
+              fill='#ff8434'
+            />
+            <path
+              id='Path_82'
+              data-name='Path 82'
+              d='M214.1,154c-7.89,4.55-12.67,4.09-17.29,3.65-6.67-.66-13-1.26-24.89,14.35l26.73,15.44c11.93-15.62,18.23-15,24.9-14.36,4.62.44,9.4.91,17.28-3.65Z'
+              transform='translate(-57.34 -66.85)'
+              fill='url(#linear-gradient)'
+            />
+            <path
+              id='Path_83'
+              data-name='Path 83'
+              d='M183.49,108.76c-9.33,5.39-15.23,4.81-20,4.36-6.33-.61-10.15-1-19.34,11l-2.86-3.57c11.93-15.62,18.23-15,24.9-14.36,4.62.44,9.4.91,17.28-3.65Z'
+              fill='#ffab57'
+            />
+            <g id='Group_3' data-name='Group 3'>
+              <path
+                id='Path_84'
+                data-name='Path 84'
+                d='M269.65,88.46l26.73,15.43c-.65-4.16-2.17-7-4.34-8.22L265.3,80.23c2.18,1.26,3.7,4.06,4.35,8.23Z'
+                transform='translate(-57.34 -66.85)'
+                fill='url(#linear-gradient-2)'
+              />
+              <path
+                id='Path_85'
+                data-name='Path 85'
+                d='M269.11,73.66a9.25,9.25,0,0,0-7.25-.67c-5.8,1.53-11.93,7.19-15.61,14.39-4,7.8-4.3,15.84-.91,22.06,3.23,5.92,3.8,11.48,1.7,16.54-2.28,5.49-7.82,10.18-11.88,12.52l26.73,15.44c4.06-2.35,9.61-7,11.89-12.53,2.09-5,1.52-10.61-1.7-16.53-3.4-6.22-3.06-14.27.91-22.07,3.67-7.2,9.8-12.85,15.6-14.39a9.33,9.33,0,0,1,7.25.67Z'
+                transform='translate(-57.34 -66.85)'
+                fill='url(#linear-gradient-3)'
+              />
+              <path
+                id='Path_86'
+                data-name='Path 86'
+                d='M204.55,93.26V87.09c4.06-2.35,9.61-7,11.89-12.53,2.09-5,1.52-10.62-1.7-16.53-3.4-6.22-3.06-14.27.91-22.07,3.67-7.2,9.8-12.85,15.6-14.39a9.33,9.33,0,0,1,7.25.67c3,1.7,5,5.36,5.82,10.75l-5.28,4c-.65-4.16-2.17-7-4.34-8.22a6.21,6.21,0,0,0-4.84-.48c-3.75,1-7.8,4.68-10.1,9.2-2.62,5.14-2.69,10.48-.2,15,4.07,7.47,4.54,15.09,1.4,22.66-4.3,10.52-13.65,16.52-16.41,18.11Z'
+                fill='#ffab57'
+              />
+            </g>
+            <g id='Group_5' data-name='Group 5'>
+              <path
+                id='Path_87'
+                data-name='Path 87'
+                d='M215.35,114.62A11.8,11.8,0,0,1,217,102.7l-26.73,15.44a11.8,11.8,0,0,0-1.65,11.92c3.42,9,15.62,20.46,33.13,31.38L248.47,146C231,135.09,218.77,123.59,215.35,114.62Z'
+                transform='translate(-57.34 -66.85)'
+                fill='url(#linear-gradient-4)'
+              />
+              <path
+                id='Path_88'
+                data-name='Path 88'
+                d='M254.43,155.75,227.7,171.19c0-26.11-27.55-68-39.86-60.85L214.57,94.9C226.88,87.79,254.43,129.64,254.43,155.75Z'
+                transform='translate(-57.34 -66.85)'
+                fill='url(#linear-gradient-5)'
+              />
+              <path
+                id='Path_89'
+                data-name='Path 89'
+                d='M128.34,45.46c-10.84,14.47,5.08,37.55,42,58.88,0-26.11-27.55-68-39.86-60.85A7.1,7.1,0,0,0,128.34,45.46Zm2.93,17.75a11.8,11.8,0,0,1,1.65-11.92,4.46,4.46,0,0,1,1.36-1.26c1.3-.75,2.9-.55,4.9.61,8.59,5,22.19,26.34,25.22,44-17.51-10.97-29.74-22.49-33.13-31.43Z'
+                fill='#ffe186'
+              />
+              <g id='Group_4' data-name='Group 4'>
+                <path
+                  id='Path_90'
+                  data-name='Path 90'
+                  d='M292.11,156.12l-26.74,15.43c-6.39,3.69-16.92,3.91-31.94-3.46l26.74-15.44c15.01,7.35,25.55,7.16,31.94,3.47Z'
+                  transform='translate(-57.34 -66.85)'
+                  fill='url(#linear-gradient-6)'
+                />
+                <path
+                  id='Path_91'
+                  data-name='Path 91'
+                  d='M260.17,152.65l-26.74,15.44c1.86-15.18,10-31.35,18.65-36.32l26.74-15.43C270.21,121.3,262,137.48,260.17,152.65Z'
+                  transform='translate(-57.34 -66.85)'
+                  fill='url(#linear-gradient-7)'
+                />
+                <path
+                  id='Path_92'
+                  data-name='Path 92'
+                  d='M301.53,127.78c-8.7-19.35-18.26-22.65-26.49-17.9l-26.73,15.43c8.22-4.74,17.79-1.44,26.48,17.91,6.61,14.71,4.53,29.07-5.71,35l26.74-15.43c10.23-5.93,12.32-20.3,5.71-35.01Z'
+                  transform='translate(-57.34 -66.85)'
+                  fill='url(#linear-gradient-8)'
+                />
+                <path
+                  id='Path_93'
+                  data-name='Path 93'
+                  d='M217.45,76.36c-8.69-19.34-18.26-22.64-26.48-17.89-11.65,6.72-20.61,29.59-20.61,45.87,19.09,11,33,11.86,41.38,7,10.24-5.9,12.32-20.26,5.71-34.98Zm-4.12,23.2a14.71,14.71,0,0,1-5.3,5.14c-6.39,3.69-16.92,3.91-31.94-3.46,1.86-15.18,10-31.35,18.65-36.32,3.1-1.79,6.25-2.12,9.18-.43s6.09,6.07,9.15,12.89c3.66,8.15,3.76,16.44.26,22.18Z'
+                  fill='#ffe186'
+                />
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
   );
 }
 
 export { Gift };
+
+// <div className={styles.gift}>
+//   <div className={styles.lid}></div>
+//   <div className={styles.body}></div>
+//   <div className={styles.lasso__first}></div>
+//   <div className={styles.lasso__second}></div>
+// </div>
